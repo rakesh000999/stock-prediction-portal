1df124ece2a1fd43514cc2c9989d93c89e0bb459
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-08-18 18:40:39 +0545

    [add] api url setup and test

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password'] 
