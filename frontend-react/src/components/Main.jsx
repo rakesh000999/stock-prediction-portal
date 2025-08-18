df25eb457be0e31845f207cfd9bdcb159e803f23
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-08-18 16:47:51 +0545

    [ADD] main component design

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Main = () => {
-  return (
-    <div className='text-light'>Main</div>
-  )
+    return (
+        <>
+            <div className='container'>
+                <div className='p-5 text-center bg-light-dark rounded'>
+                    <h1 className='text-light'>Stock Prediction Portal</h1>
+                    <p className='text-light lead'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil accusamus asperiores blanditiis molestiae ex reiciendis perferendis! Facilis atque rerum molestias quasi ad ipsam, sapiente ea distinctio placeat dolorum doloremque eligendi, ab, perferendis incidunt magnam numquam!</p>
+                    <a className='btn btn-outline-success' href="">Login</a>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Main
