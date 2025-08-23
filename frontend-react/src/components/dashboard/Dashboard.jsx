85baefe1658d600f0909952fbb326a02bcec9938
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-08-23 12:41:03 +0545

    axiosInstance

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import axiosInstance from '../../axiosInstance';
 
 const Dashboard = () => {
 
@@ -8,7 +9,7 @@ const Dashboard = () => {
     useEffect(() => {
         const fetchProtectedData = async () => {
             try {
-                const response = await axios.get('http://127.0.0.1:8000/api/v1/protected-view', {
+                const response = await axiosInstance.get('/protected-view/', {
                     headers: {
                         Authorization: `Bearer ${accessToken}`
                     }
