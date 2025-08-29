2d5ea4e628ba7f7f8e678d82753ce796ee493a04
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-08-29 23:24:38 +0545

    generate plot with AGG backends

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import os
+from django.conf import settings
 
 class StockPredictionAPIView(APIView):
     def post(self, request):
@@ -20,8 +22,27 @@ class StockPredictionAPIView(APIView):
             start = datetime(now.year - 10, now.month, now.day)
             end = now
             df = yf.download(ticker, start, end)
-            print(df)
             if df.empty:
                 return Response({'error': 'No data found for the given ticker.',
                                  'status': status.HTTP_404_NOT_FOUND})
+            df = df.reset_index()
+
+            # Generate Basic Plot
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.title(f'Closing Price of {ticker}')
+            plt.xlabel('Date')
+            plt.ylabel('Close Price')
+            plt.legend()
+
+            # Save the plot to a file
+            plot_img_path = f'{ticker}_plot.png'
+            image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+            plt.savefig(image_path)
+            plt.close()
+            image_url = settings.MEDIA_URL + plot_img_path
+            print(image_url)
+            
+
             return Response({'status': 'success', 'ticker': ticker})
