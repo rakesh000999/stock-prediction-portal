7942a8d7bc88b73e644e95f4f2bf80e834ace03f
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-08-29 13:39:26 +0545

    fetching data from yfinance

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -3,10 +3,25 @@ from rest_framework.views import APIView
 from .serializers import StockPredictionSerializer
 from rest_framework.response import Response
 from rest_framework import status
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 class StockPredictionAPIView(APIView):
     def post(self, request):
         serializer = StockPredictionSerializer(data=request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data['ticker']
+
+            # Fetch the data from yfinance
+            now = datetime.now()
+            start = datetime(now.year - 10, now.month, now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+            print(df)
+            if df.empty:
+                return Response({'error': 'No data found for the given ticker.',
+                                 'status': status.HTTP_404_NOT_FOUND})
             return Response({'status': 'success', 'ticker': ticker})
